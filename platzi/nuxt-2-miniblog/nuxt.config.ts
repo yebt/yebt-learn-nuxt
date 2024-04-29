9e719023e56074b313748f06b912d253550b1345
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-04-29 14:37:41 -0500

    feat: fix the eslint

--- platzi/nuxt-2-miniblog/nuxt.config.ts
+++ platzi/nuxt-2-miniblog/nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', "@nuxt/eslint"]
-})
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/eslint",
+    // [
+    //   "@nuxtjs/stylelint-module",
+    //   {
+    //     /* module options */
+    //   },
+    // ],
+  ],
+});
